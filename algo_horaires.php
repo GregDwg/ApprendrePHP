babd0ef6341e5f5b8fce86fb04fd6dfb478f6e6b
GregDwg <78653152+GregDwg@users.noreply.github.com>
2021-09-03 18:42:38 +0200

    Update algo_horaires.php

--- algo_horaires.php
+++ algo_horaires.php
@@ -1,6 +1,14 @@
 <?php
+$horaire =(int)readline("Veuillez indiquer votre horaire: ");
 
-horaires 
-
+if(($horaire >= 9 && $horaire < 12) || ($horaire > 14 && $horaire < 17 )) {
+    echo("le magasin est ouvert\n"); 
+}
+elseif($horaire !== is_int($horaire)) {
+    echo("Veuillez écrire un chiffre\n");
+}
+    else{
+        echo("le magasin est fermé\n");
+    }
 
 ?>
