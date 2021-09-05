67877247fa1cc9b7578710e5f5bef9b7dfdce6fb
GregDwg <78653152+GregDwg@users.noreply.github.com>
2021-09-05 20:44:02 +0200

    ajout de strtolower()
    ajout de strtolower pour prendre en charge les mots écrits en majuscule.

--- palindrome.php
+++ palindrome.php
@@ -4,6 +4,7 @@
 
 echo ("Jeu du mot: \n"); 
 $mot = readline("Veuillez entre un mot :");
+$mot = strtolower($mot);
 $palindrome = strrev($mot);
     if($mot === $palindrome){
     echo ("$palindrome est un palindrome \n"); 
