fa60655af0dc7ba404ab6c67e6c4e1cef1948310
GregDwg <78653152+GregDwg@users.noreply.github.com>
2021-09-05 20:37:48 +0200

    Jeu du palindrome

---
+++ palindrome.php
@@ -0,0 +1,16 @@
+<?php 
+
+// <------ JEU DU PALINDROME ------> 
+
+echo ("Jeu du mot: \n"); 
+$mot = readline("Veuillez entre un mot :");
+$palindrome = strrev($mot);
+    if($mot === $palindrome){
+    echo ("$palindrome est un palindrome \n"); 
+    }
+    else{
+        echo ("Ce mot n'est pas un palindrome \n");
+    }
+
+    
+?>
